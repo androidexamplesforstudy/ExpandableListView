1234da54b3940997497ee184c0f846e85712f7c7
AdamK <Adamkubarek@gmail.com>
2016-12-29 07:28:38 +0100

    init extra files

--- app/src/main/java/example/com/expandablelistview/MainActivity.java
+++ app/src/main/java/example/com/expandablelistview/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        
+
     }
 }
